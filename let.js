86915e23e6d52e0d6eb09da66f6e4e694ef7382e
Anmol Diwedi <ianmol3107@gmail.com>
2022-09-20 21:29:35 +0530

    Add files via upload

---
+++ let.js
@@ -0,0 +1,12 @@
+let count=4
+console.log(count)
+// let count=5    we can't declared any variable twice in program
+count=10       // we can only modified it, both having same variable NAMES and we have changing the VALUES like -- 4 to 10.
+console.log(count)
+function display()
+{
+    let count=40  // we can declared again only in function! and this variable is totally different from other one.
+    console.log(count)
+}
+
+display()
