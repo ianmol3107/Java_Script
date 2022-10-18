3a57144b6a8f40fa2e343145458229e36a768882
Anmol Diwedi <ianmol3107@gmail.com>
2022-10-18 19:51:41 +0530

    Create check_age.js

---
+++ check_age.js
@@ -0,0 +1,7 @@
+let age= prompt("Enter your age? ");
+if(age>10 && age<20){
+  console.log("Yes your age lies between 10 and20");
+}
+else{
+  console.log("YOur age doesn't lies between 10 and 20");
+}
