fd357be4a84b0187666d07d7f3f49f8767e1d9b5
Anmol Diwedi <ianmol3107@gmail.com>
2022-10-20 21:03:37 +0530

    Create Check_divisible_by_2_3.js

---
+++ Check_divisible_by_2_3.js
@@ -0,0 +1,8 @@
+let num = prompt("Enter your number: ");
+num = Number.parseInt(num);
+if(num%2==0 && num%3==0){
+  console.log("Number is divisible by 2 and 3");
+}
+else{
+  console.log("Number is not divisible by 2 and 3! Try Again");
+}
