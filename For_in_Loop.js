bb98e7b237e228da676a8c8874afdde4c32b4ba2
Anmol Diwedi <ianmol3107@gmail.com>
2022-10-23 10:26:19 +0530

    Create For_in_Loop.js

---
+++ For_in_Loop.js
@@ -0,0 +1,17 @@
+let obj={
+  Anmol : 90,
+  Akhil : 80,
+  Pooja : 84,
+  Sivika: 70,
+  Ritika: 60,
+  Tripti: 50
+}
+// FOR IN LOOP
+for(let i in obj){
+  console.log("Marks of "+ i + " are "+ obj[i]);
+}
+
+// FOR OF LOOP mainly use for ARRAYS AND STRING.
+for(let i of "Anmol"){
+  console.log(i);
+}
