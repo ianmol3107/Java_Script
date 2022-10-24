88dd804702a35f7c1c683f4d0612befd601c2613
Anmol Diwedi <ianmol3107@gmail.com>
2022-10-24 22:42:01 +0530

    Create While_loop.js

---
+++ While_loop.js
@@ -0,0 +1,16 @@
+// While loop
+a = prompt("Enter your number: ");
+a = Number.parseInt(a);
+let i = 1;
+while(i<=a){
+  console.log(i);
+  i++;
+}
+// Do While loop
+num = prompt("Enter your number: ");
+num = Number.parseInt(num);
+let j = 10;
+do{
+  console.log(j);
+  j++;
+}while(j<=num)
