fab3b5a28a8c8157cc2db97d332692c9e2da09e4
Anmol Diwedi <ianmol3107@gmail.com>
2022-10-26 22:32:25 +0530

    Create Marks_of_student_in_object_using_for_loop.js

---
+++ Marks_of_student_in_object_using_for_loop.js
@@ -0,0 +1,9 @@
+let marks = {
+  Anmol: 90,
+  Akhil: 80,
+  Sakshi: 80,
+  Aviral: 70
+}
+for(let i=0; i<Object.keys(marks).length; i++){
+  console.log("The Marks of "+ object.keys(marks)[i]+ " are " + marks[object.key(marks)[i]]);
+}
