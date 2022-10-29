58a0b5f46ec6cff29ee31e67cdc1556a2221794f
Anmol Diwedi <ianmol3107@gmail.com>
2022-10-29 18:55:32 +0530

    Create Strings_literals.js

---
+++ Strings_literals.js
@@ -0,0 +1,10 @@
+let name1 = "Anmol";
+let name2 = 'Akhil';
+// Akhil is a friend of Anmol.
+// let sentence = `${name2} is a friend of ${name1}`;
+let sentence = `${name2} is friend of ${name1} and "Rahul" also friend of both`;
+
+console.log(sentence);
+
+let sen = `This is "a' ANMOL`; // here we can use double and single quotes easily.
+console.log(sen);
